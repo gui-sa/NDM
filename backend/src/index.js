bbd9ac089a0dbf006ff923258a717c6b10a3be61
gui-sa <guime.sa9@gmail.com>
2023-02-09 19:49:46 -0300

    Iniciando servidor... Nodemon e pacotes instalados

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+const PORT = 5000;
 
 app.get("/api", (req,res)=>{
+    
     res.json({"users":["userOne","userTwo","UserThree","MoreJson"]})
 })
 
-app.listen(5000, ()=>{console.log("Server Started on http://localhost:5000")})
+app.listen(5000, ()=>{console.log(`Server Started on http://localhost:${PORT}`)})
