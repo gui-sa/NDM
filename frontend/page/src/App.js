777efb695fece48de16157b59edbb8bb84d72339
gui-sa <guime.sa9@gmail.com>
2023-02-12 19:09:31 -0300

    icone do menu foi arrumado

--- frontend/page/src/App.js
+++ frontend/page/src/App.js
@@ -14,7 +14,6 @@ function App() {
           <Route path='/jogadores' element={<Jogadores/>}/>
           <Route path='/times' element={<Times/>}/>
         </Routes>
-        <Footer />
       </Router >
     </div>
 )}
--- frontend/page/src/containers/footer/Footer.jsx
+++ frontend/page/src/containers/footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './footer.css';
+import { MapTrifold } from "phosphor-react";
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className='sport__footer'>
+        <p>Crido por Guilherme Salomão Agostini</p>
+        <div className='sport__footer-address'>
+          <MapTrifold size={32} color="#ffffff" weight="bold" />
+          <address></address>
+        </div>
     </div>
   )
 }
